e36b85455bb8fadff73e46b3cea3f8e7e7961656
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-01-11 21:06:42 -0300

    ✨ feat: auth (login and register user)

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,62 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.messages import constants
+from django.contrib import messages
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def register(request):
-  return render(request, 'register.html')
+  if request.method == 'GET':
+    if request.user.is_authenticated: 
+      return redirect('/')
+    return render(request, 'register.html')
+
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+
+    print(username)
+    print(email)
+    print(senha)
+
+    if len(username.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0:
+      messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+      return redirect('/auth/register')
+
+    user = User.objects.filter(username=username)
+
+    if(user.exists()):
+      messages.add_message(request, constants.ERROR, 'Usuário já existe')
+      return redirect('/auth/register')
+    
+    try:
+        user = User.objects.create_user(username=username,
+                                        email=email,
+                                        password=senha)
+        user.save()
+        messages.add_message(request, constants.SUCCESS, 'Cadastro realizado com sucesso')
+        return redirect('/auth/login')
+    except:
+        messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+        return redirect('/auth/register')
 
 def login(request):
-  return render(request, 'login.html')
+  if request.method == 'GET':
+    if request.user.is_authenticated: 
+      return redirect('/')
+    return render(request, 'login.html')
+
+  elif request.method == 'POST':
+    print('')
+    username = request.POST.get('username')
+    senha = request.POST.get('senha')
+    
+    user = auth.authenticate(username=username, password=senha)
+    
+    if not user:
+      messages.add_message(request, constants.ERROR, 'Usuário não existe')
+      return redirect('/auth/login')
+    
+    auth.login(request, user)
+    return redirect('/')
--- imobi/settings.py
+++ imobi/settings.py
@@ -1,6 +1,7 @@
 import os
 
 from pathlib import Path
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,3 +118,13 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django messages treatment
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
